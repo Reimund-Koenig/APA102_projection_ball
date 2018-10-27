bebd0f2f8aebd25f4fde6cd95db080aeca61b3fb
Reimund König <reimund@gmx.net>
2018-10-27 13:17:31 +0200

    init MQTT

---
+++ ball/ball.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import threading
+import paho.mqtt.client as mqtt
+
+
+MQTT_SERVER = "localhost"
+MQTT_PATH = "power"
+
+continueRun = True
+list_input = []
+class MyMQTTClass(mqtt.Client):
+
+    def on_connect(self, mqttc, obj, flags, rc):
+        self.subscribe(MQTT_PATH, 0)
+        print("rc: "+str(rc))
+
+    def on_message(self, mqttc, obj, msg):
+        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+    def on_publish(self, mqttc, obj, mid):
+        print("mid: "+str(mid))
+
+    #def on_subscribe(self, mqttc, obj, mid, granted_qos):
+    #    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+    #def on_log(self, mqttc, obj, level, string):
+    #    print(string)
+
+    def run(self):
+        self.connect(MQTT_SERVER, 1883, 60)
+
+        rc = 0
+        while rc == 0:
+            rc = self.loop()
+        return rc
+
+class UserInput(threading.Thread):
+
+    def __init__(self, id, name):
+        threading.Thread.__init__(self)
+        self.threadID = id
+        self.name = name
+
+    # @staticmethod
+    def run(self):
+        global list_input
+        global continueRun
+        while continueRun:
+            eingabe = input()
+            if eingabe == "exit":
+                continueRun = False
+            list_input.append(eingabe)
+
+
+class HandleUserInput(threading.Thread):
+    def __init__(self, id, name):
+        threading.Thread.__init__(self)
+        self.threadID = id
+        self.name = name
+
+    # @staticmethod
+    def run(self):
+        global list_input
+        global continueRun
+        while continueRun:
+            if len(list_input) > 0:
+                print(list_input.pop())
+
+
+# Create new threads
+thread1 = UserInput(1, "Thread-1")
+thread2 = HandleUserInput(2, "Thread-2")
+
+mqttc = MyMQTTClass()
+rc = mqttc.run()
+
+print("rc: "+str(rc))
+
+# Start new Threads
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+print("Exiting Main Thread")
+
+
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# NUM_LED = 144
+# filename = "pics/line.txt"
+# content = ""
+# pixel = 0
+#
+# with open(filename) as f:
+#     content = f.readlines()
+#
+# turnsPerSecond = 50
+# stepsPerTurn = 360
+# oneMicrosecond = seconds / 1000000.0
+# turn_rate = 1.0/turnsPerSecond                      # 50 Hz = 0,02 seconds per round
+#
+# # 360 vertical pixes * 50 turns = 18000 color changes per second = 1/18000 = 0.00005555555
+# step_rate = 1.0/(stepsPerTurn * turnsPerSecond)
+# while 1:
+#     nextTurn = time.time() + turn_rate  # 1267919090.35663390159606933594
+#
+#     # show LEDs for one turn
+#     for idx, line in enumerate(content):
+#         color = hex(int(line, 16))
+#         strip.set_pixel_rgb(pixel, color)
+#         if idx % 76 == 0:
+#             while time.time() < nextSwitchTime:
+#                 time.sleep(oneMicrosecond)
+#             nextSwitchTime = time.time + step_rate
+#
+#     # wait for next turn
+#     while time.time < turn_rate:
+#         time.sleep(oneMicrosecond)
---
+++ ball/mqttConnector.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+import threading
+
+MQTT_SERVER = "localhost"
+MQTT_PATH = "power"
+loop = ""
+
+
+
